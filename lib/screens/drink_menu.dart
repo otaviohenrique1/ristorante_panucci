60397fabf08b324900191032a5742467a3ca4f4e
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-07 22:19:20 -0300

    commit 5

--- lib/screens/drink_menu.dart
+++ lib/screens/drink_menu.dart
@@ -10,34 +10,56 @@ class DrinkMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
+    return Padding(
+      padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16),
+              child: Text(
+                "Bebidas",
+                style: TextStyle(
+                  fontFamily: "Caveat",
+                  fontSize: 32,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return DrinkItem(
+                  imageURI: items[index]["image"],
+                  itemTitle: items[index]["name"],
+                  itemPrice: items[index]["price"],
+                );
+              },
+              childCount: items.length,
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+          ),
+        ],
       ),
-      itemBuilder: (context, index) {
-        return DrinkItem(
-          imageURI: items[index]["image"],
-          itemTitle: items[index]["name"],
-          itemPrice: items[index]["price"],
-        );
-      },
-      itemCount: items.length,
     );
   }
 }
 
 /*
-const SliverToBoxAdapter(
-  child: Padding(
-    padding: EdgeInsets.only(bottom: 16),
-    child: Text(
-      "Bebidas",
-      style: TextStyle(
-        fontFamily: "Caveat",
-        fontSize: 32,
-      ),
-      textAlign: TextAlign.center,
-    ),
+GridView.builder(
+  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 2,
   ),
-),
+  itemBuilder: (context, index) {
+    return DrinkItem(
+      imageURI: items[index]["image"],
+      itemTitle: items[index]["name"],
+      itemPrice: items[index]["price"],
+    );
+  },
+  itemCount: items.length,
+)
 */
