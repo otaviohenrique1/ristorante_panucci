984e80c9a28626f337079a75370566d6df35c2ec
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-08 11:59:59 -0300

    commit 7

---
+++ lib/screens/checkout.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:ristorante_panucci/cardapio.dart';
+import 'package:ristorante_panucci/components/order_item.dart';
+
+import '../components/main_drawer.dart';
+
+class Checkout extends StatelessWidget {
+  const Checkout({super.key});
+
+  final List items = pedido;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ristorante Panucci"),
+        backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+      ),
+      drawer: const MainDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: CustomScrollView(
+          slivers: [
+            const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 16),
+                child: Text(
+                  "Pedido",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return OrderItem(
+                    imageURI: items[index]["image"],
+                    itemTitle: items[index]["name"],
+                    itemPrice: items[index]["price"],
+                  );
+                },
+                childCount: items.length,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ristorante_panucci/components/main_drawer.dart';
+import 'package:ristorante_panucci/screens/checkout.dart';
 import 'package:ristorante_panucci/screens/drink_menu.dart';
 import 'package:ristorante_panucci/screens/food_menu.dart';
 import 'package:ristorante_panucci/screens/highlights.dart';
@@ -39,7 +40,7 @@ class _HomeState extends State<Home> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const Home();
+            return const Checkout();
           }));
         },
         child: const Icon(Icons.point_of_sale),
