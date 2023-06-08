fcb95283196ea9ad1ce131c84e7093de69b0c53c
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-08 12:25:27 -0300

    commit 9

--- lib/screens/drink_menu.dart
+++ lib/screens/drink_menu.dart
@@ -11,7 +11,7 @@ class DrinkMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
       child: CustomScrollView(
         slivers: <Widget>[
           const SliverToBoxAdapter(
@@ -38,11 +38,17 @@ class DrinkMenu extends StatelessWidget {
               },
               childCount: items.length,
             ),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount:
+                  (MediaQuery.of(context).orientation == Orientation.landscape)
+                      ? 3
+                      : 2,
               crossAxisSpacing: 8,
               mainAxisSpacing: 8,
-              childAspectRatio: 158 / 194,
+              childAspectRatio:
+                  (MediaQuery.of(context).orientation == Orientation.landscape)
+                      ? 1.2
+                      : 158 / 194,
             ),
           ),
         ],
