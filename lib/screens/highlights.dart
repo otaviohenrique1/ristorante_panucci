3221dfa59ce1c8045199d9732f55cbc9081cd7bc
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-08 12:38:53 -0300

    commit 10

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -26,20 +26,71 @@ class Highlights extends StatelessWidget {
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return HighlightItem(
-                  imageURI: items[index]["image"],
-                  itemTitle: items[index]["name"],
-                  itemPrice: items[index]["price"],
-                  itemDescription: items[index]["description"],
-                );
-              },
-              childCount: items.length,
-            ),
-          ),
+          (MediaQuery.of(context).orientation == Orientation.landscape)
+              ? _LandscapeList(items: items)
+              : _PortraitList(items: items),
         ],
+      ),
+    );
+  }
+}
+
+class _PortraitList extends StatelessWidget {
+  const _PortraitList({
+    // ignore: unused_element
+    super.key,
+    required this.items,
+  });
+
+  final List items;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return HighlightItem(
+            imageURI: items[index]["image"],
+            itemTitle: items[index]["name"],
+            itemPrice: items[index]["price"],
+            itemDescription: items[index]["description"],
+          );
+        },
+        childCount: items.length,
+      ),
+    );
+  }
+}
+
+class _LandscapeList extends StatelessWidget {
+  const _LandscapeList({
+    // ignore: unused_element
+    super.key,
+    required this.items,
+  });
+
+  final List items;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return HighlightItem(
+            imageURI: items[index]["image"],
+            itemTitle: items[index]["name"],
+            itemPrice: items[index]["price"],
+            itemDescription: items[index]["description"],
+          );
+        },
+        childCount: items.length,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 1,
+        // childAspectRatio: 1.2,
       ),
     );
   }
